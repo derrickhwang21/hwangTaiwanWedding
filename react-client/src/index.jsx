cf828d29ccd4a1ec43d987d867ca9f686f6c0def
dhwang21 <hwang.derrick@gmail.com>
2019-02-22 13:11:22 -0800

    initial build

--- react-client/src/index.jsx
+++
@@ -1,6 +0,0 @@
-import React, { Component } from 'react';
-import 'semantic-ui-css/semantic.min.css';
-import ReactDOM from 'react-dom';
-
-import App from './Components/App';
-ReactDOM.render(<App />, document.getElementById('app'));
